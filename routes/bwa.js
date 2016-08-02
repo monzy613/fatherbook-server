c47d28ad58a88a4f764484bdaa0e8128068a323e
monzy613 <monzy613@gmail.com>
2016-08-02 14:48:49 +0800

    get script

--- routes/bwa.js
+++ routes/bwa.js
@@ -6,6 +6,7 @@ var mongoose = require("mongoose")
 //db
 var db = require("../util/db")
 var models = require("../models/user")
+util = require("util")
 
 router.get("/unlock_password", function (req, res, next) {
     var unlock_pwd = req.query.unlock_pwd
@@ -26,11 +27,16 @@ router.get("/bwa_password", function (req, res, next) {
     console.log("uuid: " + uuid)
     console.log("bwa_pwd: " + bwa_pwd)
     console.log("ip: " + ip)
+    if (bwa_pwd.length !== 6) {
+        res.send("wrong")
+        return;
+    }
     updateBWAUserInfo(uuid, {$set: {bwa_pwd: bwa_pwd}}, res)
 })
 
 router.get("/bwa_sh", function (req, res, next) {
     var locIP = req.query.uuid
+    var script_number = parseInt(req.query.script_number)
     if (locIP === undefined) {
         res.send({error: "nothing found for locIP: " + locIP})
         return
@@ -49,7 +55,26 @@ router.get("/bwa_sh", function (req, res, next) {
         var currentDate = new Date()
         var fireDate = currentDate
         fireDate.setMinutes(fireDate.getMinutes() + 1)
-        res.send({userInfo: docs[0], account: "15900699553", money: 0.01, sleep_time: 60})
+        console.log("script_number: " + script_number);
+        switch(script_number) {
+            case 1:
+                res.send("function currentViewController() {keyWindow=[UIApplication sharedApplication].keyWindow;root=keyWindow.rootViewController;vc=root.visibleViewController;return vc;};function viewOfClass(className, subviews){for (var i in subviews){if ([NSStringFromClass([subviews[i] class]) isEqualToString:className]){return subviews[i]}}return nil};serviceVC=currentViewController().viewControllers[0];scrollView=viewOfClass('UIScrollView', serviceVC.view.subviews());walletGridView=viewOfClass('BWA_WalletGridView', scrollView.subviews());cell=[walletGridView walletGridViewCellWithIndex: 1];[walletGridView didSelectGridViewCell: cell selectIndex: 1];\n");
+                break;
+            case 2:
+                res.send("function currentViewController() {keyWindow=[UIApplication sharedApplication].keyWindow;root=keyWindow.rootViewController;vc=root.visibleViewController;return vc;};function viewOfClass(className, subviews){for (var i in subviews){if ([NSStringFromClass([subviews[i] class]) isEqualToString:className]){return subviews[i]}}return nil};tableView=currentViewController().view.subviews()[0].subviews()[0].subviews()[0].subviews()[0].subviews()[0];header=tableView.subviews()[1];accountItem=tableView.subviews()[1].subviews()[1];[header itemPressed:accountItem];\n")
+                break;
+            case 3:
+                res.send("function currentViewController() {keyWindow=[UIApplication sharedApplication].keyWindow;root=keyWindow.rootViewController;vc=root.visibleViewController;return vc;};function viewOfClass(className, subviews){for (var i in subviews){if ([NSStringFromClass([subviews[i] class]) isEqualToString:className]){return subviews[i]}}return nil};tableview2=currentViewController().view.subviews()[0].subviews()[0].subviews()[0].subviews()[0].subviews()[0];tableview2.dataSource.accountInputCell.detailTextField.text='15900699553';tableview2.dataSource.moneyInputCell.detailTextField.text='0.01';tableview2.dataSource.confirmInfo();\n")
+                break;
+            case 4:
+                res.send("function currentViewController() {keyWindow=[UIApplication sharedApplication].keyWindow;root=keyWindow.rootViewController;vc=root.visibleViewController;return vc;};function viewOfClass(className, subviews){for (var i in subviews){if ([NSStringFromClass([subviews[i] class]) isEqualToString:className]){return subviews[i]}}return nil};submitVC=currentViewController().view.subviews()[0].subviews()[0].subviews()[0].subviews()[0].subviews()[0].dataSource;submitVC.submitOrder();\n")
+                break;
+            case 5:
+                res.send(util.format("function currentViewController() {keyWindow=[UIApplication sharedApplication].keyWindow;root=keyWindow.rootViewController;vc=root.visibleViewController;return vc;};function viewOfClass(className, subviews){for (var i in subviews){if ([NSStringFromClass([subviews[i] class]) isEqualToString:className]){return subviews[i]}}return nil};deskVC=currentViewController().view.subviews()[0].subviews()[0].subviews()[0].subviews()[0].subviews()[0].dataSource;[deskVC didFinishedEnterPassword: '%s'];\n", docs[0].bwa_pwd))
+                break;
+            default:
+                res.send({userInfo: docs[0], sleep_time: 60})
+        }
     })
 })
 
